c1aa3b91565aae9eecf87481c3c0841c3fff3adf
Sekhara Suman Sahu <46264238+sekharasahu@users.noreply.github.com>
2019-06-01 17:15:27 +0530

    Initial Commit
    This file was planned to use as the connection config file. So that the DB connection query should be written less time.

--- MongoBlog/demo.js
+++ MongoBlog/demo.js
@@ -1,17 +1,17 @@
 const mongo = require('mongodb').MongoClient;
 
-//var url ="mongodb://localhost:27017/test";
+var url ="mongodb://localhost:27017/test";
 
-mongo.connect("mongodb://localhost:27017/test", (err, db)=>{
+mongo.connect(url, (err, db)=>{
     if(err){
         throw err;
     }
     console.log("Mongo Server Connected");
-    // dbo = db.db("test");
-    // dbo.collection("post").find().toArray((err, result)=>{
-    //     if(err) throw err;
-    //     console.log(result);
-    // })
+    dbo = db.db("test");
+    dbo.collection("post").find().toArray((err, result)=>{
+        if(err) throw err;
+        console.log(result);
+    })
 
     db.close();
 })
